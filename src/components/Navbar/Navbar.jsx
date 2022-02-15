a8178eedc2bef148cb1abd89b517f19dc6d04b0b
Laurent-Ch <chamouleau.laurent@gmail.com>
2022-02-15 16:22:07 +0100

    redux implemented, login and logout work

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import Cookies from 'js-cookie';
 
 const Navbar = () => {
+
+  const handleLogout = () => {
+  Cookies.remove('userJwt');
+  }
+
   return (
     <div className='navbar'>
       <nav>
       <Link to="/register" className="navbar__element">Register</Link>
       <Link to="/login" className="navbar__element">Login</Link>
-      <Link to="/logout" className="navbar__element">Logout</Link>
+      <Link to="/" className="navbar__element" onClick={() => handleLogout()}>Logout</Link>
       </nav>
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
-import Logout from "./components/Home/Logout/Logout";
 
 const App = () => {
 
@@ -18,7 +17,6 @@ const App = () => {
           <Route path="/" element={<Home />}/>
           <Route path="/register" element={<RegisterForm />}/>
           <Route path="/login" element={<Login />}/>
-          <Route path="/logout" element={<Logout />}/>
         </Routes>
       </Router>
     </div>

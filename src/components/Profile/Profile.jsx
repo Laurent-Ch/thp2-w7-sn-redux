cc4499964f5939e14ea0fe4b2a00f70f16cd2ce7
Laurent-Ch <chamouleau.laurent@gmail.com>
2022-02-16 21:40:57 +0100

    modifying current user profile implemented

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,6 +7,7 @@ import {Navigate} from "react-router-dom";
 
   const Profile = () => {
   
+  // Getting the user's data
   const [loaded, setLoaded] = useState(false);
   const [userData, setUserData] = useState(null);
   const loggedStatus = useSelector(state => state.logged);
@@ -31,6 +32,33 @@ import {Navigate} from "react-router-dom";
 
   !loaded && getUserData();
 
+  // Updating ther user's data
+  const [newUsername, setNewUsername] = useState();
+  const [newDescription, setNewDescription] = useState();
+  const handleNewUsername = (e) => {
+    setNewUsername(e.target.value);
+  }
+  const handleNewDescription = (e) => {
+    setNewDescription(e.target.value);
+  }
+
+  const data = {
+    username: newUsername,
+    description: newDescription
+  };
+
+  const editUserData = () => { 
+    console.log(userData.id)
+    fetch(`http://localhost:1337/users/me`, {
+      method: 'put',
+      headers: {
+        'Authorization': `Bearer ${userCookie}`, 
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  }  
+
   if (!loggedStatus.logged) {
     return <Navigate to='/' />
   }
@@ -41,8 +69,19 @@ import {Navigate} from "react-router-dom";
     return (
       <div>
         <h1>Profile</h1>
-        <div>Username: {userData.username}</div>
-        <div>Email: {userData.email}</div>
+        <div>Current username: {userData.username}</div>
+        <div>Current email: {userData.email}</div>
+        <div>Current description: {userData.description}</div>
+        <h2>Editing your profile</h2>
+        <form className='profile__edit'> 
+        <label htmlFor="username">New username</label>
+        <input id="username" type="text" onChange={handleNewUsername}/>
+
+        <label htmlFor="password">New description</label>
+        <input id="password" type="text" onChange={handleNewDescription}/>
+
+        <button type="button" className='form-submit-btn' onClick={() => editUserData()}>edit</button>
+        </form> 
       </div>
   )};
 
